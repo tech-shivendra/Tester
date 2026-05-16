7b8cff3ab19f4b7005896c926b1cdb85d605ee02
shivendra <techshivendraa@gmail.com>
2026-05-16 10:26:30 +0530

    Added Gemini Model to Find Bugs

---
+++ js/env.js
@@ -0,0 +1,4 @@
+export const ENV = {
+    GEMINI_API_KEY: "AIzaSyA6rPkHu1Od1_tXphptkgxzsDFmkQhYtrI",
+    FIREBASE_API_KEY: "AIzaSyA0e2zJkUeXDmw8MiEzlnO0sozV0dItCBA" // Firebase keys are usually public, but we can put it here if preferred
+};
