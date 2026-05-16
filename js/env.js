b87e4ca86ebe781f82894ac29b03ca2f4f3f28cf
tech-shivendra <techshivendraa@gmail.com>
2026-05-16 12:47:40 +0530

    Change GEMINI_API_KEY in env.js
    Updated GEMINI_API_KEY to a new value.

--- js/env.js
+++ js/env.js
@@ -1,4 +1,4 @@
 export const ENV = {
-    GEMINI_API_KEY: "AIzaSyDjOL5S8SkYqVA56ZhDfqVuK-SjzkwLoC8",
+    GEMINI_API_KEY: "AIzaSyBI5X8sNDqjTnH_gSuLPw6fIKLNfS5WvU4",
     FIREBASE_API_KEY: "AIzaSyA0e2zJkUeXDmw8MiEzlnO0sozV0dItCBA" // Firebase keys are usually public, but we can put it here if preferred
 };
