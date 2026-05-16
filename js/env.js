7e34953fdd2c1fddb5f68b8dd4f881894499dd25
tech-shivendra <techshivendraa@gmail.com>
2026-05-16 14:25:22 +0530

    Update env.js

--- js/env.js
+++ js/env.js
@@ -1,4 +1,4 @@
 export const ENV = {
-    GEMINI_API_KEY: "AIzaSyBI5X8sNDqjTnH_gSuLPw6fIKLNfS5WvU4",
+    GEMINI_API_KEY: "AIzaSyB43EIQFVCh0nlRd5J_BG6ZYeLD_usAHAM",
     FIREBASE_API_KEY: "AIzaSyA0e2zJkUeXDmw8MiEzlnO0sozV0dItCBA" // Firebase keys are usually public, but we can put it here if preferred
 };
